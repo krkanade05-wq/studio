07a85c0f48aada6bcac051d3d5a00e8cf34948fb
Kartik Kanade <kanadekartik05@gmail.com>
2025-09-21 08:13:39 +0000

    im unable to sign in with google directly

--- src/components/auth/google-signin.tsx
+++ src/components/auth/google-signin.tsx
@@ -1,31 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { getGoogleSignInOffer } from '@/app/actions/auth';
 import { GoogleSignInButton } from './google-signin-button';
-import { Skeleton } from '../ui/skeleton';
 
 export function GoogleSignIn() {
-  const [offerGoogleSignIn, setOfferGoogleSignIn] = useState<boolean | null>(
-    null
-  );
-
-  useEffect(() => {
-    getGoogleSignInOffer().then((result) => {
-      setOfferGoogleSignIn(result.offerGoogleSignIn);
-      if (!result.offerGoogleSignIn) {
-        console.log(
-          `Google Sign-In not offered. Reason: ${result.reason}`
-        );
-      }
-    });
-  }, []);
-
-  if (offerGoogleSignIn === null) {
-    return (
-      <Skeleton className="h-10 w-full" />
-    );
-  }
-
-  return <GoogleSignInButton offerGoogleSignIn={offerGoogleSignIn} />;
+  return <GoogleSignInButton offerGoogleSignIn={true} />;
 }

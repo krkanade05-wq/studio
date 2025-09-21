cc77e385a5c9118e6ca7a150d01958f82663a0bd
Kartik Kanade <kanadekartik05@gmail.com>
2025-09-21 18:57:47 +0000

    AIzaSyALTWBrB0s_-BrXXd5KbmBN6roSOloyIFE this is my api key use it wisely

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,9 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
   ],
   model: 'googleai/gemini-2.5-flash',
 });
